f2375f6c2715e22951493ee82008f92b579a38eb
Lo Fl <lflora@cub.uca.edu>
2026-03-18 10:43:00 -0500

    the security risk was already fixed, git is being awful

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,16 @@
 import { useQuery, useRealm } from "@realm/react";
-import Mapbox from "@rnmapbox/maps";
+import Mapbox, { MapView } from "@rnmapbox/maps";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { MapMarker } from "../../src/models/MapMarker";
-import { StyleSheet, Text } from "react-native";
-
 
 export default function Index() {
   const realm = useRealm();
   const markerItems = useQuery(MapMarker);
 
-
   return (
-    <SafeAreaView className="flex-1" >
-
-      <Mapbox.MapView style={styles.container}>
-        <Mapbox.Camera
-        zoomLevel={12}
-        centerCoordinate={[-74.0060, 40.7128]}/>
-
-      </Mapbox.MapView>
+    <SafeAreaView className="flex-1">     <Mapbox.MapView style={styles.container} styleURL={Mapbox.StyleURL.Outdoors} />
     </SafeAreaView>
   );
 }
@@ -26,5 +18,5 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    }
+  },
 });
